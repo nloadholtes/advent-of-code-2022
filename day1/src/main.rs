567b7b67fbe6a245457241af5d40632671cb501d
Nick Loadholtes <nick@ironboundsoftware.com>
2022-12-03 13:11:48 -0500

    Reading from command line, debug showing

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,17 +1,31 @@
+use std::env;
+
 fn main() -> Result<(), std::io::Error> {
-    let input = read_input()?;
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    let input = read_input(&args[1])?;
     let lines = input.split("\n\n");
 
+    let mut max = Vec::new();
     for group in lines {
-        println!("Group:");
+        let mut sum = 0;
         for line in group.lines() {
-            println!("   - Got line: {}", line);
+            let value = line.parse::<u64>().unwrap();
+            // println!("   - {value}");
+            sum += value;
         }
+        // // println!("Group sum: {sum}");
+        // if sum > max {
+        //     max = sum;
+        // }
+        max.push(sum);
     }
+    max.sort_by_key(|&v| v);
+    println!("{}", max[0]);
     Ok(())
 }
 
-fn read_input() -> Result<String, std::io::Error> {
+fn read_input(input_file: &String) -> Result<String, std::io::Error> {
     // now from the `fs-err` crate, rather than `std::fs`
-    fs_err::read_to_string("src/input.txt")
+    fs_err::read_to_string(input_file)
 }

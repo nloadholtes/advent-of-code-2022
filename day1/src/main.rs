0251520c8522a202bb4e51ae11415c2ee098d274
Nick Loadholtes <nick@ironboundsoftware.com>
2022-12-03 11:58:29 -0500

    Adding in a better read_to_string impl

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let input = read_input().unwrap();
+    println!("{input}");
+}
+
+fn read_input() -> Result<String, std::io::Error> {
+    // now from the `fs-err` crate, rather than `std::fs`
+    fs_err::read_to_string("src/input.txt")
 }
